4f568d89d3070cc4d43dfb14e0c83d570081754f
Loubna Soleil <loubna.rezougue@gmail.com>
2019-03-18 18:00:44 +0100

    Unimplemented methods

--- src/linda/shm/CentralizedLinda.java
+++ src/linda/shm/CentralizedLinda.java
@@ -1,5 +1,7 @@
 package linda.shm;
 
+import java.util.Collection;
+
 import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
@@ -9,6 +11,60 @@ public class CentralizedLinda implements Linda {
 	
     public CentralizedLinda() {
     }
+
+	@Override
+	public void write(Tuple t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Tuple take(Tuple template) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tuple read(Tuple template) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tuple tryTake(Tuple template) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tuple tryRead(Tuple template) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Tuple> takeAll(Tuple template) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Tuple> readAll(Tuple template) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eventRegister(eventMode mode, eventTiming timing, Tuple template, Callback callback) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void debug(String prefix) {
+		// TODO Auto-generated method stub
+		
+	}
 
     // TO BE COMPLETED
 
