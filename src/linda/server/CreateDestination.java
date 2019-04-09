fed23dc6a31d8598795ea01d5c417f7b738fe58c
Loubna Soleil <loubna.rezougue@gmail.com>
2019-04-09 08:58:11 +0200

    Buildpath issues

--- src/linda/server/CreateDestination.java
+++ src/linda/server/CreateDestination.java
@@ -1,9 +1,19 @@
 package linda.server;
-import javax.naming.* ;
+
+import org.objectweb.joram.client.jms.Destination;
+import org.objectweb.joram.client.jms.Topic;
+import org.objectweb.joram.client.jms.admin.AdminModule;
+import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
 
 public class CreateDestination {
 	public static void main(String args[]) throws Exception {
-	    
-	   
-	  }
+		// Connecting to JORAM server:
+	    AdminModule.connect("root", "root", 60);
+
+	    // Creating the JMS administered objects:        
+	    javax.jms.ConnectionFactory connFactory =
+	      TcpConnectionFactory.create("localhost", 16010);
+
+	    Destination destination = Topic.create(0);
+	}
 }
